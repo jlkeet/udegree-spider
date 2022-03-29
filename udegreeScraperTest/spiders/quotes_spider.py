e4680118336446013a6c0ec3d4aa296cfcdd12c2
Jackson Keet <jackson.keet@mac.com>
2022-03-29 22:30:01 +1300

    Added comments

--- udegreeScraperTest/spiders/quotes_spider.py
+++ udegreeScraperTest/spiders/quotes_spider.py
@@ -24,9 +24,8 @@ class QuotesSpider(scrapy.Spider):
              yield response.follow(href, self.parse_dir_contents)
 
     def parse_dir_contents(self, response):
-            self.log("starting scrape 23")
+            self.log("Starting Scraper")
             newUrl = str(response)[5:-1] + '/eligibility-and-requirements'
-            #self.log(newUrl)
             item =  {
                     'name': response.xpath('//meta[@name="short_title"]/@content').get(), 
                     'title': response.xpath('//meta[@name="code"]/@content').get(),
@@ -35,33 +34,11 @@ class QuotesSpider(scrapy.Spider):
                     # 'department': response.xpath('//h1/text()').get().strip(),
                     'points': response.xpath('//meta[@name="points"]/@content').get(),
                     'stage': response.xpath('//meta[@name="level"]/@content').get()[-1],
-                    # 'restrictions': response.xpath('//p[@class="prerequisite"]//i/text()').getall()[0].strip()
                      }
             yield scrapy.Request(url=newUrl, callback=self.parse_requirements, meta={'item': item})
 
     def parse_requirements(self, response):
+        self.log("Parsing Requirements")
         item = response.meta['item']
         item['requirements'] = response.xpath("//td[1]//text()").getall()
         yield item
-
-
-    # def start_requests(self):
-    #     urls = [
-    #         'https://handbook.unimelb.edu.au/search?study_periods%5B%5D=all&area_of_study%5B%5D=all&types%5B%5D=subject&year=2022&level_type%5B%5D=all&campus_and_attendance_mode%5B%5D=all&org_unit%5B%5D=all&page=1&sort=external_code%7Casc',
-    #     ]
-    #     for url in urls:
-    #         yield scrapy.Request(url=url, callback=self.parse)
-
-    # def parse(self, response):
-    #         self.log("starting scrape")
-
-    #         yield { # possibly best to loop through for each of these.
-    #             'name': response.xpath('//span[@class="search-result-item__code"]/text()').getall(), # can strip later
-    #             'title': response.xpath('//div[@class="search-result-item__name"]/h3/text()').getall(), # can strip later
-    #             'desc': response.xpath('//p[@class="description"]/text()').getall()[0].strip(), # can strip later
-    #             'faculties': response.xpath('//h2[@class="b8"]/text()').get().strip(' ')[-1],
-    #             'department': response.xpath('//h1/text()').get().strip(),
-    #             'points': response.xpath('//div[@class="search-result-item__meta-secondary"]/text()').getall()[0].strip(), # can strip later
-    #             'stage': response.xpath('//div[@class="search-result-item__meta-secondary"]/text()').getall()[1].strip()[-3],
-    #             'restrictions': response.xpath('//p[@class="prerequisite"]//i/text()').getall()[0].strip() # this is the hard part, this will give us the restrictions but we must create logic to parse them with normal language. eeeek.
-    #         }
